958022d0e8d551772955e38ff8778e22fda6cd18
Gautam <gautamsharma2813@gmail.com>
2022-12-10 12:11:49 +0530

    first commit

---
+++ part1/part1/src/Total.js
@@ -0,0 +1,10 @@
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.exercises1} + {propsexercises2} +{" "}
+      {props.exercises3}
+    </p>
+  );
+};
+
+export default Total;
